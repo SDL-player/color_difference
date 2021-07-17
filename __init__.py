53f850215b1fd4b1ccb16143e64c6d9a719d376a
SDL-player <83915344+SDL-player@users.noreply.github.com>
2021-07-16 22:49:10 -0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,51 @@
+from ctypes import *
+
+functions = CDLL("functions.dll")
+
+rgb2srgb = functions.rgb_to_srgb
+
+srgb2linear = functions.srgb_to_linear
+
+linear2xyz = functions.linear_to_xyz
+
+xyz2Lab = functions.xyz_to_lab
+
+delta_cie_2000_ = functions.delta_e_cie2000_
+
+delta_cie_2000_.restype = c_double
+
+
+class Color_Diff:
+    
+    def __init__(self, a, b):
+        
+        """Calculate the delta_cie_2000 between two values in rgb format."""
+        
+        self.a = a
+        self.b = b
+        
+    def calculate(self):
+        
+        arr_ = (c_double * len(self.a))(*self.a)
+        
+        rgb2srgb(arr_)
+        
+        srgb2linear(arr_)
+        
+        linear2xyz(arr_)
+        
+        xyz2Lab(arr_)
+        
+        arr_2 = (c_double * len(self.b))(*self.b)
+        
+        rgb2srgb(arr_2)
+        
+        srgb2linear(arr_2)
+        
+        linear2xyz(arr_2)
+        
+        xyz2Lab(arr_2)
+        
+        delta = delta_cie_2000_(arr_, arr_2)
+        
+        return delta
