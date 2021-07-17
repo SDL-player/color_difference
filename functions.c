7dc29a973519eafcea057950a74080efe1effb5b
SDL-player <83915344+SDL-player@users.noreply.github.com>
2021-07-17 12:03:10 -0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 
-const double pi = 3.141592653589793;
-const double e = 216.0 / 24389.0;
-const double k =  24389.0 / 27.0;
+#define pi 3.141592653589793;
+#define e 216.0 / 24389.0;
+#define k 24389.0 / 27.0;
 
 double radians(double deg){
 
