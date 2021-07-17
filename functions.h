7214675e8975cbd84942b7b6e9e7958f5f3f17ac
SDL-player <83915344+SDL-player@users.noreply.github.com>
2021-07-17 15:28:54 -0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -4,18 +4,18 @@
 #define e 216.0 / 24389.0
 #define k 24389.0 / 27.0
 
-double radians(double deg);
+extern double radians(double deg);
 
-double degress(double rad);
+extern double degress(double rad);
 
-void rgb_to_srgb(double rgb[3]);
+extern void rgb_to_srgb(double rgb[3]);
 
-void srgb_to_linear(double srgb[3]);
+extern void srgb_to_linear(double srgb[3]);
 
-void linear_to_xyz(double linear[3]);
+extern void linear_to_xyz(double linear[3]);
 
-double f_x(double x);
+extern double f_x(double x);
 
-void xyz_to_lab(double xyz[3]);
+extern void xyz_to_lab(double xyz[3]);
 
-double delta_e_cie2000_(double Lab1[3], double Lab2[3]);
+extern double delta_e_cie2000_(double Lab1[3], double Lab2[3]);
